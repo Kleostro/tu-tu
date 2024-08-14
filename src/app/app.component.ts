4788a5bf107a55dd3d1f65eb0aba59eff3d3bdce
Max <133934232+Kleostro@users.noreply.github.com>
2024-08-14 22:05:12 +0300

    fix: tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { NzButtonModule } from 'ng-zorro-antd/button';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
-  imports: [RouterOutlet, NzButtonModule],
+  imports: [RouterOutlet],
 })
 export class AppComponent {}
