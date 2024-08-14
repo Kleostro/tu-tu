a9bb8e69d63ddd07da202ff44065240835f272f0
Max <133934232+Kleostro@users.noreply.github.com>
2024-08-14 22:00:02 +0300

    chore: setup ngZorro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzButtonModule],
 })
 export class AppComponent {}
