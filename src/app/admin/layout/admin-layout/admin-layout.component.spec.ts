f7657f6272e3f53b3af19ace81f367878bd0c409
Max <133934232+Kleostro@users.noreply.github.com>
2024-08-16 20:58:17 +0300

    feat: implement layout on the admin page

--- src/app/admin/layout/admin-layout/admin-layout.component.spec.ts
+++ src/app/admin/layout/admin-layout/admin-layout.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 
 import { AdminLayoutComponent } from './admin-layout.component';
 
@@ -9,6 +10,7 @@ describe('AdminLayoutComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AdminLayoutComponent],
+      providers: [{ provide: ActivatedRoute, useValue: {} }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AdminLayoutComponent);
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,34 @@
 import { Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./admin/layout/admin-layout/admin-layout.component').then((c) => c.AdminLayoutComponent),
+    children: [
+      {
+        path: 'carriages',
+        loadComponent: () => import('./admin/pages/carriages/carriages.component').then((c) => c.CarriagesComponent),
+        title: 'Admin | carriages',
+      },
+      {
+        path: 'routes',
+        loadComponent: () => import('./admin/pages/routes/routes.component').then((c) => c.RoutesComponent),
+        title: 'Admin | routes',
+      },
+      {
+        path: 'schedule',
+        loadComponent: () => import('./admin/pages/schedule/schedule.component').then((c) => c.ScheduleComponent),
+        title: 'Admin | schedule',
+      },
+      {
+        path: 'stations',
+        loadComponent: () => import('./admin/pages/stations/stations.component').then((c) => c.StationsComponent),
+        title: 'Admin | stations',
+      },
+    ],
+    title: 'Admin',
+  },
+];
 
 export default routes;
