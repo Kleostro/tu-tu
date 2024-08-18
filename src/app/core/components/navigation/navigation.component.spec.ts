1d462f06244798ae99d6050288f2f9f457f558b6
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-08-18 20:04:28 +0300

    fix: tests

--- src/app/core/components/navigation/navigation.component.spec.ts
+++ src/app/core/components/navigation/navigation.component.spec.ts
@@ -1,6 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 
+import { MessageService } from 'primeng/api';
+
+import LocalStorageData from '../../models/store.model';
+import { LocalStorageService } from '../../services/local-storage/local-storage.service';
 import { NavigationComponent } from './navigation.component';
 
 describe('NavigationComponent', () => {
@@ -10,7 +15,31 @@ describe('NavigationComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NavigationComponent],
-      providers: [{ provide: ActivatedRoute, useValue: {} }],
+      providers: [
+        { provide: ActivatedRoute, useValue: {} },
+        { provide: HttpClient, useValue: {} },
+        { provide: MessageService, useValue: {} },
+        {
+          provide: LocalStorageService,
+          useValue: {
+            getValueByKey: (key: string): unknown => {
+              if (key === 'token') {
+                return 'mockToken';
+              }
+              if (key === 'email') {
+                return 'mockEmail';
+              }
+              return null;
+            },
+            addValueByKey: (): void => {},
+            removeValueByKey: (): void => {},
+            clear: (): void => {},
+            save: (): void => {},
+            saveCurrentUser: (): void => {},
+            init: (): LocalStorageData => ({}),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavigationComponent);
