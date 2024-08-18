edb48bdb6846a8795400b15c9caedfe7c00a2029
Max <133934232+Kleostro@users.noreply.github.com>
2024-08-18 03:44:06 +0300

    feat: implement marker linking on the map

--- src/app/admin/constants/initial-map-state.ts
+++ src/app/admin/constants/initial-map-state.ts
@@ -1,3 +1,5 @@
+import { AddLayerObject } from 'maplibre-gl';
+
 export const INITIAL_MAP_STATE = { lng: 2.349014, lat: 48.864716, zoom: 10 };
 export const MARKER_PARAMS = {
   height: 40,
@@ -5,4 +7,14 @@ export const MARKER_PARAMS = {
   offset: 20,
   max_width: '5rem',
   color: '#3B82F6',
+};
+
+export const LINE_LAYER: AddLayerObject = {
+  id: 'line',
+  type: 'line',
+  source: 'line',
+  paint: {
+    'line-color': MARKER_PARAMS.color,
+    'line-width': 4,
+  },
 };
--- src/app/admin/pages/stations/stations.component.ts
+++ src/app/admin/pages/stations/stations.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+import { StationsService } from '@/app/api/stationsService/stations.service';
 
 import { CreateStationFormComponent } from '../../components/create-station-form/create-station-form.component';
 import { MapComponent } from '../../components/map/map.component';
@@ -6,9 +10,12 @@ import { MapComponent } from '../../components/map/map.component';
 @Component({
   selector: 'app-stations',
   standalone: true,
-  imports: [MapComponent, CreateStationFormComponent],
+  imports: [MapComponent, CreateStationFormComponent, AsyncPipe],
   templateUrl: './stations.component.html',
   styleUrl: './stations.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StationsComponent {}
+export class StationsComponent {
+  private stationsService = inject(StationsService);
+  public allStations = toSignal(this.stationsService.getStations());
+}
