ab10f6b85a1c82792c46c941d916e4dc9cea505a
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-08-18 03:19:31 +0300

    refactor: remove unnecessary import

--- src/app/core/pages/not-found/not-found.component.ts
+++ src/app/core/pages/not-found/not-found.component.ts
@@ -1,11 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { ButtonModule } from 'primeng/button';
-
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
