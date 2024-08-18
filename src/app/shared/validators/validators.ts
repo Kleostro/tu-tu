95b05b35e5950efe7e8733f8d4c69f1a88edcd54
katyastan <134015338+katyastan@users.noreply.github.com>
2024-08-19 00:50:55 +0300

    feat: add error messages with symbols excluding space around

--- src/app/shared/validators/validators.ts
+++ src/app/shared/validators/validators.ts
@@ -5,3 +5,32 @@ export const passwordMatchValidator: ValidatorFn = (control: AbstractControl): V
   const confirm = control.get('confirm');
   return password && confirm && password.value === confirm.value ? null : { passwordMismatch: true };
 };
+
+// export function minTrimmedLengthValidator(minLength: number): ValidatorFn {
+//   return (control: AbstractControl): ValidationErrors | null => {
+//     const value = control.value as string | null;
+//     if (value !== null && value.trim().length < minLength) {
+//       return { minTrimmedLength: { requiredLength: minLength, actualLength: value.trim().length } };
+//     }
+//     return null;
+//   };
+// }
+
+// export function minTrimmedLengthValidator(minLength: number): ValidatorFn {
+//   return (control: AbstractControl): ValidationErrors | null => {
+//     const value = control.value;
+//     if (typeof value === 'string' && value.trim().length < minLength) {
+//       return { minTrimmedLength: { requiredLength: minLength, actualLength: value.trim().length } };
+//     }
+//     return null;
+//   };
+// }
+
+export function minTrimmedLengthValidator(minLength: number): ValidatorFn {
+  return ({ value }: AbstractControl): ValidationErrors | null => {
+    if (typeof value === 'string' && value.trim().length < minLength) {
+      return { minTrimmedLength: { requiredLength: minLength, actualLength: value.trim().length } };
+    }
+    return null;
+  };
+}
