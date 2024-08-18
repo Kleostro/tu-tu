225d912643d272935d8ce303de158d0582faf660
katyastan <134015338+katyastan@users.noreply.github.com>
2024-08-19 00:52:15 +0300

    refactor: remove comments

--- src/app/shared/validators/validators.ts
+++ src/app/shared/validators/validators.ts
@@ -6,26 +6,6 @@ export const passwordMatchValidator: ValidatorFn = (control: AbstractControl): V
   return password && confirm && password.value === confirm.value ? null : { passwordMismatch: true };
 };
 
-// export function minTrimmedLengthValidator(minLength: number): ValidatorFn {
-//   return (control: AbstractControl): ValidationErrors | null => {
-//     const value = control.value as string | null;
-//     if (value !== null && value.trim().length < minLength) {
-//       return { minTrimmedLength: { requiredLength: minLength, actualLength: value.trim().length } };
-//     }
-//     return null;
-//   };
-// }
-
-// export function minTrimmedLengthValidator(minLength: number): ValidatorFn {
-//   return (control: AbstractControl): ValidationErrors | null => {
-//     const value = control.value;
-//     if (typeof value === 'string' && value.trim().length < minLength) {
-//       return { minTrimmedLength: { requiredLength: minLength, actualLength: value.trim().length } };
-//     }
-//     return null;
-//   };
-// }
-
 export function minTrimmedLengthValidator(minLength: number): ValidatorFn {
   return ({ value }: AbstractControl): ValidationErrors | null => {
     if (typeof value === 'string' && value.trim().length < minLength) {
