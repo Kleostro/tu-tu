94885c696558ffa54406748761086d6194e889eb
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-08-18 03:06:27 +0300

    feat: implement not found page

--- src/app/core/pages/not-found/not-found.component.ts
+++ src/app/core/pages/not-found/not-found.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
