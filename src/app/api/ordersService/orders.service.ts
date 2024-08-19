907ef73ae66bcbbabd1c39294734437def5ee7b6
ki8vi <oneofbln@gmail.com>
2024-08-19 19:28:02 +0600

    feat: add orders service

---
+++ src/app/api/ordersService/orders.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { Order, OrderId, OrderRequest, User } from '../models/order';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrdersService {
+  private httpClient = inject(HttpClient);
+  private ORDER_ENDPOINT = 'order';
+
+  public getAllOrders(): Observable<Order[]> {
+    return this.httpClient.get<Order[]>(this.ORDER_ENDPOINT);
+  }
+
+  public makeOrder(order: OrderRequest): Observable<OrderId> {
+    return this.httpClient.post<OrderId>(this.ORDER_ENDPOINT, order);
+  }
+
+  public cancelOrder(orderId: number): Observable<object> {
+    return this.httpClient.delete<object>(`${this.ORDER_ENDPOINT}/${orderId}`);
+  }
+
+  public getAllUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(`users`);
+  }
+}
