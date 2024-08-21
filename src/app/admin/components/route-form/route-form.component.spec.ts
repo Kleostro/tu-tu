d58fdeb428b6849d2bdda9793b56bfb9dfdcb0d4
Max <133934232+Kleostro@users.noreply.github.com>
2024-08-21 21:14:28 +0300

    fix: tests

--- src/app/admin/components/route-form/route-form.component.spec.ts
+++ src/app/admin/components/route-form/route-form.component.spec.ts
@@ -1,4 +1,7 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MessageService } from 'primeng/api';
 
 import { RouteFormComponent } from './route-form.component';
 
@@ -9,6 +12,7 @@ describe('RouteFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouteFormComponent],
+      providers: [provideHttpClient(), { provide: MessageService, useValue: {} }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RouteFormComponent);
