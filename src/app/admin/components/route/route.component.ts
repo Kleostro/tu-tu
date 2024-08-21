bac7de9fa68346887a7c741ab70dbb6d541ad3d9
Max <133934232+Kleostro@users.noreply.github.com>
2024-08-21 15:45:28 +0300

    feat: route-managment-actions

--- src/app/admin/components/route/route.component.ts
+++ src/app/admin/components/route/route.component.ts
@@ -1,17 +1,24 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, inject, input, Output } from '@angular/core';
+
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 import { RouteResponse } from '@/app/api/models/route';
+import { RouteService } from '@/app/api/routeService/route.service';
 import { StationsService } from '@/app/api/stationsService/stations.service';
 
 @Component({
   selector: 'app-route',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, RippleModule],
   templateUrl: './route.component.html',
   styleUrl: './route.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RouteComponent {
   public stationsService = inject(StationsService);
+  public routeService = inject(RouteService);
   public route = input<RouteResponse | null>(null);
+
+  @Output() public openDeleteConfirm: EventEmitter<number> = new EventEmitter<number>();
 }
--- src/app/admin/components/routes-list/routes-list.component.ts
+++ src/app/admin/components/routes-list/routes-list.component.ts
@@ -1,19 +1,64 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+  OnDestroy,
+  signal,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { Subscription } from 'rxjs';
 
 import { RouteResponse } from '@/app/api/models/route';
 import { Station } from '@/app/api/models/stations';
+import { RouteService } from '@/app/api/routeService/route.service';
+import { ModalService } from '@/app/shared/services/modal/modal.service';
 
 import { RouteComponent } from '../route/route.component';
 
 @Component({
   selector: 'app-routes-list',
   standalone: true,
-  imports: [RouteComponent],
+  imports: [RouteComponent, ButtonModule, RippleModule],
   templateUrl: './routes-list.component.html',
   styleUrl: './routes-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RoutesListComponent {
+export class RoutesListComponent implements OnDestroy {
+  public routeService = inject(RouteService);
+  public modalService = inject(ModalService);
   public routes = input<RouteResponse[]>([]);
   public stations = input<Station[]>([]);
+  public deletionRouteId = signal<number>(NaN);
+  public isRouteDeleted = signal(false);
+  private subscription = new Subscription();
+
+  @ViewChild('deleteRouteConfirm') public deleteRouteConfirm!: TemplateRef<unknown>;
+
+  public handleOpenDeleteConfirm(id: number): void {
+    this.deletionRouteId.set(id);
+    this.modalService.contentWidth$$.set('40%');
+    this.modalService.openModal(this.deleteRouteConfirm, `Delete route ${id}?`);
+  }
+
+  public deleteRoute(): void {
+    this.isRouteDeleted.set(true);
+    this.subscription.add(
+      this.routeService.deleteRoute(this.deletionRouteId()).subscribe(() =>
+        this.routeService.getAllRoutes().subscribe(() => {
+          this.isRouteDeleted.set(false);
+          this.deletionRouteId.set(NaN);
+          this.modalService.closeModal();
+        }),
+      ),
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
