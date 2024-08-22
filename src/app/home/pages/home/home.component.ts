6253b50707f1f393600ec4d94bb1315c98c73df4
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-08-22 20:21:37 +0300

    feat: display result list on main page

--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { ResultListComponent } from '../../components/result-list/result-list.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ResultListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
