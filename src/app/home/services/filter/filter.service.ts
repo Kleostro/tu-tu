10d591210708961b079412d3e2770209ecaf51cf
ki8vi <oneofbln@gmail.com>
2024-08-24 04:46:51 +0600

    feat: add styles

--- src/app/home/services/filter/filter.service.ts
+++ src/app/home/services/filter/filter.service.ts
@@ -1,7 +1,9 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 
 import { Route } from '@/app/api/models/search';
 
+import { SearchService } from '@/app/api/searchService/search.service';
+import { OverriddenHttpErrorResponse } from '@/app/api/models/errorResponse';
 import { GroupedRoutes, TripPoints } from '../../model/groupedRoutes';
 
 @Injectable({
@@ -10,15 +12,20 @@ import { GroupedRoutes, TripPoints } from '../../model/groupedRoutes';
 export class FilterService {
   public availableRoutesGroup$$ = signal<GroupedRoutes>({});
   public tripPoints$$ = signal<TripPoints | null>(null);
+  private searchService = inject(SearchService);
+
+  constructor() {
+    this.startFakeSearch();
+  }
 
   private generateAvaillableRoutesGroup(routes: Route[], targetDate: Date): GroupedRoutes {
-    const targetDateUnix = targetDate.toISOString();
+    const targetDateISO = targetDate.toISOString();
     const groupedRoutes: GroupedRoutes = {};
 
     routes.forEach(({ id: routeId, schedule, path, carriages }) => {
       schedule.forEach(({ rideId, segments }) => {
         segments
-          .filter(({ time }) => new Date(time[0]).toISOString() > targetDateUnix)
+          .filter(({ time }) => new Date(time[0]).toISOString() > targetDateISO)
           .forEach((segment) => {
             const departureDateTime = new Date(segment.time[0]);
             const departureDate = departureDateTime.toISOString().split('T')[0];
@@ -37,7 +44,32 @@ export class FilterService {
           });
       });
     });
-
+    
     return groupedRoutes;
   }
+
+  private startFakeSearch(): void {
+    const date = Date.now();
+    const searchPrms = {
+      fromLatitude: 33.6253023965961,
+      fromLongitude: -62.87929972362075,
+      toLatitude: -64.91480386879022,
+      toLongitude: -169.82655423507208,
+      time: date.toString(),
+    };
+    this.searchService.search(searchPrms).subscribe({
+      next: (res) => {
+        this.availableRoutesGroup$$.set(this.generateAvaillableRoutesGroup(res.routes, new Date(date)));
+        this.tripPoints$$.set({
+          from: res.from.city,
+          to: res.to.city,
+        })
+      },
+      error: (err: OverriddenHttpErrorResponse) => {
+        throw Error(err.message);
+      },
+    })
+  }
 }
+
+
