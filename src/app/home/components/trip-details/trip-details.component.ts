4cbcfc2163170d763a018e2d361386611dce56bf
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-08-27 00:45:46 +0300

    feat: scroll to first user station

--- src/app/home/components/trip-details/trip-details.component.ts
+++ src/app/home/components/trip-details/trip-details.component.ts
@@ -1,5 +1,14 @@
 import { DatePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 
 import { TimelineModule } from 'primeng/timeline';
 
@@ -14,13 +23,26 @@ import { StationInfo } from '../../models/stationInfo.model';
   styleUrl: './trip-details.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TripDetailsComponent implements OnInit {
+export class TripDetailsComponent implements OnInit, AfterViewInit {
+  @ViewChildren('userMarker') private markers!: QueryList<ElementRef<HTMLSpanElement>>;
+
   @Input() public trip: CurrentRide | null = null;
 
   public events: StationInfo[] = [];
 
   public ngOnInit(): void {
     this.setData();
+  }
+
+  public ngAfterViewInit(): void {
+    this.scrollToFirstUserStation();
+  }
+
+  private scrollToFirstUserStation(): void {
+    const firstUserStation = this.markers.find((marker) => marker.nativeElement.classList.contains('selected'));
+    if (firstUserStation) {
+      firstUserStation.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 
   private setData(): void {
