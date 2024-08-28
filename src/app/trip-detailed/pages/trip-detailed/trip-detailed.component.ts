f03572a13abe842fe2f8a4a27ed734f2043b3dcb
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-08-29 01:18:10 +0300

    feat: display chosen trip time

--- src/app/trip-detailed/pages/trip-detailed/trip-detailed.component.ts
+++ src/app/trip-detailed/pages/trip-detailed/trip-detailed.component.ts
@@ -1,11 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+
+import { RoutingService } from '@/app/core/services/routing/routing.service';
+import { CurrentRide } from '@/app/home/models/currentRide.model';
+import { ResultListService } from '@/app/home/services/result-list/result-list.service';
+
+import { TripTimelineComponent } from '../../../home/components/trip-timeline/trip-timeline.component';
 
 @Component({
   selector: 'app-trip-detailed',
   standalone: true,
-  imports: [],
+  imports: [TripTimelineComponent],
   templateUrl: './trip-detailed.component.html',
   styleUrl: './trip-detailed.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TripDetailedComponent {}
+export class TripDetailedComponent implements OnInit {
+  private resultListService = inject(ResultListService);
+  private routingService = inject(RoutingService);
+  public tripItem!: CurrentRide | undefined;
+
+  public ngOnInit(): void {
+    const rides = this.resultListService.currentResultList$$();
+    this.tripItem = rides.find((ride) => ride.rideId === +this.routingService.currentRideId$$());
+  }
+}
