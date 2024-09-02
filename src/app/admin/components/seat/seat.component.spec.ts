e7e2de1cb654e5e32f33385c40ad34fe7ce85388
Max <133934232+Kleostro@users.noreply.github.com>
2024-09-02 19:57:32 +0300

    fix: tests

--- src/app/admin/components/seat/seat.component.spec.ts
+++ src/app/admin/components/seat/seat.component.spec.ts
@@ -1,3 +1,4 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SeatComponent } from './seat.component';
@@ -9,6 +10,7 @@ describe('SeatComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SeatComponent],
+      providers: [provideHttpClient()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SeatComponent);
